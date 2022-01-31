9f45db135726ae5b32ee292642ac0515c17f5ac7
SimoAndreas1 <98739308+SimoAndreas1@users.noreply.github.com>
2022-01-31 11:30:48 +0200

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,52 @@
+# Harjutus 01
+# Simo Andreas Novikov
+# 31.01.2022
+
+print("\"Cabernet Sauvignon\" või 'Cabernet Sauvignon'?")
+
+
+print("  @..@")
+print(" (----)")
+print("( \__/ )")
+print('^^ "" ^^')
+
+#Sisesta ruudu külje pikkus: 10
+#Vastus: ruudu pindala on 100 cm^2
+
+a = int(input("Sisesta ruudu külje pikkus: "))
+p = a * a
+print("Vastus: ruudu pindala on",p,"cm^2") 
+
+
+
+
+'''
+nimi = input("Lisa oma nimi: ")
+print("Tere", nimi+"!") # Pluss ei lisa tühikut
+vanus = int(input("Lisa oma vanus: ")) 
+vanuste_vahe = 17-vanus
+print("Meie vanuste vahe on: ", vanuste_vahe,"aastat") 
+'''
+'''
+a=12
+b=16
+a = b
+S=a*b
+print("Pindala",S,"ruutmeetrit") 
+p= 2*(a+b)
+print("ümbermõõt",p,"meetrit")
+
+print(2*3)
+#P=2*(A+B)
+'''
+
+'''
+print("tere*maailm") #sõne
+print('tere sa ka') #String
+print(2000) #int / täisarv
+print(2.5) #float / komaarv
+print('Ütle "Tere Maailm"')
+print("Ütle \"Tere Maailm\"") #escape string
+
+print(type(200)) #näitab andmetüüpi
+'''
