cced5d322ee54e57b2c5a58cf9569f40b179cb91
SimoAndreas1 <98739308+SimoAndreas1@users.noreply.github.com>
2022-02-01 13:49:14 +0200

    Add files via upload

---
+++ 02.py
@@ -0,0 +1,66 @@
+import math
+# Harjutus 02
+# Simo Andreas Novikov
+# 01.02.2022
+#POOLELI"
+
+#arvusüsteemid
+minutid = int(input("Sisesta minutid: "))
+tund =
+minut =
+print(minutid, "minutit on", tund,":", minut)
+
+
+
+
+
+
+
+
+#hüpotenuus
+a,b=16,9
+c=math.sqrt(a**2+b**2)
+print(
+
+
+
+
+
+
+
+#rulluisutajad
+
+kiirus = 29.9
+aeg = 24
+
+summa = (kiirus/aeg)
+print(aeg, "minutiga jõuab",summa,"km") 
+
+
+
+
+#pitsa
+
+
+inimesed = 3
+hind = 12.90
+jootraha = 0.1
+summa = (hind+hind*jootraha)/inimesed
+print(inimesed, "igaüks peab maksma",summa,"€") 
+
+
+
+#3 toote hind
+hind = 36.75
+soodus = 0.4
+kogus = 3
+summa = (hind-(hind*soodus))*kogus
+print(kogus, "toote hind on",summa,"€")
+
+
+
+
+# Kolmnurga ümbermõõt
+a,b,c = 15,16,18
+p=a+b+c
+print("Kolmnurga ümbermõõt: ",p)
