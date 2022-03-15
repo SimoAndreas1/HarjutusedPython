fa24f10f3a73f6d82f6587cd6c9a3e6cf37d7da0
SimoAndreas1 <98739308+SimoAndreas1@users.noreply.github.com>
2022-03-15 11:05:44 +0200

    Update KT2.py

--- KT2.py
+++ KT2.py
@@ -4,86 +4,41 @@
 
 import random
 correct = 0
+game = 1
+kusimused = 10
 
-while True:
-    küsimused = int(input("Lisa mitu küsimust sa soovid vastata: "))
-    raskus = input("Lisa küsimuste raskus millele vastata: algaja või raske: ")
-math = input ("Vali liitmine või lahutamine: ")
+while game==1:
+    for i in range(kusimused):
+        num1 = random.randint(1, 10)
+        num2 = random.randint(1, 10)
+        arv = random.randint(1, 2)
+        if arv==1:
+            liitmine=num1 + num2
+            vastus = int(input(f"{num1}+{num2}= "))
+            if int(vastus) == liitmine:
+                print("Õige")
+                correct += 1
+            else:
+                print("Vale")
+                
+        if arv==2:
+            lahutamine=num1 - num2
+            vastus = int(input(f"{num1}-{num2}= "))
+            if int(vastus) == lahutamine:
+                print("Õige")
+                correct += 1
+            else:
+                print("Vale")
+                
+    print("Sinult küsiti", kusimused, "küsimust. Sa said", correct, "neist õigesti.")
 
-if raskus == "algaja":
-    for i in range(küsimused):
-        if math == "liitmine":
-            alg1 = random.randint(1, 10)
-            alg2 = random.randint(1, 10)
-            prod = alg1 + alg2
-            
-            AlgAns = input("Mis on " + str(alg1) + " plus " + str(alg2) + "? ")
-            
-            if int(AlgAns) == prod:
-                print("Vastus on õige -- hästi.\n")
-                correct += 1
-            else:
-                print("Ei, vastus on ",prod)
-                
-        elif math == "lahutamine":
-            alg1 = random.randint(1,10)
-            alg2 = random.randint(1,10)
-            prod = alg1 - alg2
-            
-            AlgAns = input("Mis on " + str(alg1) + " miinus " + str(alg2) + "? ")
-            
-            if int(AlgAns) == prod:
-                print("Vastus on õige -- hästi.\n")
-                correct += 1
-            else:
-                print("Ei, vastus on ",prod)
-    
-elif raskus == "raske":
-    for i in range(küsimused):
-        if math == "liitmine":
-            Ras1 = random.randint(1, 100)
-            Ras2 = random.randint(1, 100)
-            prod = Ras1 + Ras2
-                
-            RasAns = input("Mis on " + str(Ras1) + " plus " + str(Ras2) + "? ")
-                
-            if int(RasAns) == prod:
-                print("Vastus on õige -- hästi.\n")
-                correct += 1
-            else:
-                print("Ei, vastus on ",prod)
-                    
-        elif math == "lahutamine":
-            Ras1 = random.randint(1,100)
-            Ras2 = random.randint(1,100)
-            prod = Ras1 - Ras2
-                
-            RasAns = input("Mis on " + str(Ras1) + " miinus " +str(Ras2) + "? ")
-                
-            if int(RasAns) == prod:
-                print("Vastus on õige -- hästi.\n")
-                correct += 1
-            else:
-                print("Ei, vastus on ",prod)
-                    
-else:
-    print("Palun vali algaja või raske.\n")
-            
-            
-    
-print("\nMa küsisin sinult", küsimused, "küsimust. Sa said ", correct, " neist õigesti.")
-    
-if correct / küsimused > 2/3:
-    print("hästi tehtud!\n")
-elif correct / küsimused > 1/3:
-    print("Sul on vaja harjutada.\n")
-else:
-    print("Sul on tõesti abi vaja, küsi õpetajatelt!\n")
+    if correct / kusimused > 0.63:
+        print(f"tulemus: {(correct/kusimused)*100}%. Hästi tehtud!")
+    else:
+        print(f"tulemus: {(correct/kusimused)*100}%. Sul on abi vaja!")
         
-restart = input("Kas sa soovid jälle proovida? Y/N: ")
-if restart == "Y":
-    continue
-elif restart == "N":
-    break
-else:
-    print("Palun vali Y või N")
+
+    valik=input("Kas soovid jätkata? (j/e)")
+    if valik == "e":
+        game=0
+print("Tänan mängimast!")
